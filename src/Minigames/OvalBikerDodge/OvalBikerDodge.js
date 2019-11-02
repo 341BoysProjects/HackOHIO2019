57b4afaeb3e2824b145c4234e3df1529c7cd84e8
olearyryan08 <olearyryan324@gmail.com>
2019-11-02 15:31:48 -0400

    working on oval

--- src/Minigames/OvalBikerDodge/OvalBikerDodge.js
+++ src/Minigames/OvalBikerDodge/OvalBikerDodge.js
@@ -1,13 +1,22 @@
+let width = innerWidth;
+let height = innerHeight;
+
+
 let frame;
 let SCENE_W = 1600;
 let SCENE_H = 800;
+let img;
 
-
+function preload() {
+    img = loadImage('./Oval_Picture.png')
+}
 function setup() {
-    createCanvas(innerWidth,innerHeight);
-    // frame = loadImage('
+    createCanvas(width,height);
+    frame = loadImage('./Oval_Picture.png');
 }
 
 function draw() {
     ellipse(50,50,50,50);
+    console.log('draw');
+    image(frame,0,0);
 }
