958bad1f6f7380f9ff9ea77f417200df221ccbce
Evan <evy0311@gmail.com>
2019-11-02 15:28:56 -0400

    WOrking on world

--- src/world/overWorld.js
+++ src/world/overWorld.js
@@ -1,17 +1,45 @@
-let bg;
-let y = 0;
+var ghost;
+var bg;
+var frame;
+
 let width = innerWidth;
 let height = innerHeight;
+//the scene is twice the size of the canvas
+var SCENE_W = 1600;
+var SCENE_H = 800;
+
 
 function setup() {
-  // The background image must be the same size as the parameters
-  // into the createCanvas() method. In this program, the size of
-  // the image is 720x400 pixels.
-  bg = loadImage('/src/world/Map-overview.png');
   createCanvas(width, height);
+
+
+  frame = loadImage('/src/world/Map-overview-min.png');
 }
 
 function draw() {
-  background(bg);
+  background(255, 255, 255);
 
+
+  //a camera is created automatically at the beginning
+
+  //.5 zoom is zooming out (50% of the normal size)
+  if(mouseIsPressed)
+    camera.zoom = 0.5;
+  else
+    camera.zoom = 1;
+
+  //set the camera position to the ghost position
+  camera.position.x = ghost.position.x;
+  camera.position.y = ghost.position.y;
+
+
+
+
+
+
+  //I can turn on and off the camera at any point to restore
+  //the normal drawing coordinates, the frame will be drawn at
+  //the absolute 0,0 (try to see what happens if you don't turn it off
+  camera.off();
+  image(frame, 0, 0);
 }
