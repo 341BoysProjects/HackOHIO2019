fbfed1ad57ec894e138dd48ba9bc5fddfef7b391
olearyryan08 <olearyryan324@gmail.com>
2019-11-02 16:37:33 -0400

    Update OvalBikerDodge.js

--- src/Minigames/OvalBikerDodge/OvalBikerDodge.js
+++ src/Minigames/OvalBikerDodge/OvalBikerDodge.js
@@ -19,8 +19,29 @@ function setup() {
 }
 
 function draw() {
-    background(oval);
-    ellipse(xPos,yPos,50,50);
-    xPos+=1;
-    yPos+=1;
-}
+    background(world);
+  
+    nut.velocity.x = (camera.mouseX-nut.position.x)/20;
+    nut.velocity.y = (camera.mouseY-nut.position.y)/20;
+  
+    camera.position.x = nut.position.x;
+    camera.position.y = nut.position.y;
+  
+    //limit the ghost movements
+    if(nut.position.x < 0)
+      nut.position.x = 0;
+    if(nut.position.y < 0)
+      nut.position.y = 0;
+    if(nut.position.x > SCENE_W)
+      nut.position.x = SCENE_W;
+    if(nut.position.y > SCENE_H)
+      nut.position.y = SCENE_H;
+  
+    drawSprite(nut);
+  
+    //I can turn on and off the camera at any point to restore
+    //the normal drawing coordinates, the frame will be drawn at
+    //the absolute 0,0 (try to see what happens if you don't turn it off
+    // camera.off();
+    
+  }
