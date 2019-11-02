6660a26e8610adf30b38e48f047cb5357c98c3ad
olearyryan08 <olearyryan324@gmail.com>
2019-11-02 16:07:05 -0400

    Update OvalBikerDodge.js

--- src/Minigames/OvalBikerDodge/OvalBikerDodge.js
+++ src/Minigames/OvalBikerDodge/OvalBikerDodge.js
@@ -4,20 +4,19 @@ let height = innerHeight;
 let SCENE_W = 1600;
 let SCENE_H = 800;
 let oval;
-let x = width/2;
-let y = height/2;
+let xPos = width/2;
+let yPos = height/2;
 
 function preload() {
     oval = loadImage('Oval_Picture.png')
 }
 function setup() {
     createCanvas(width,height);
-
 }
 
 function draw() {
     background(oval);
-    ellipse(x,y,50,50);
-    x+=1;
-    y+=1;
+    ellipse(xPos,yPos,50,50);
+    xPos+=1;
+    yPos+=1;
 }
