d447e286a96deef5b70ab71044a01cdd73cde7ef
Evan <evy0311@gmail.com>
2019-11-02 15:33:00 -0400

    removed graphics

--- src/world/overWorld.js
+++ src/world/overWorld.js
@@ -1,24 +1,43 @@
 var ghost;
 var bg;
 var frame;
-
-let width = innerWidth;
-let height = innerHeight;
 //the scene is twice the size of the canvas
 var SCENE_W = 1600;
 var SCENE_H = 800;
 
 
 function setup() {
-  createCanvas(width, height);
+  createCanvas(800, 400);
 
+  //create a sprite and add the 3 animations
+  ghost = createSprite(400, 200, 50, 100);
 
-  frame = loadImage('/src/world/Map-overview-min.png');
+  var myAnimation = ghost.addAnimation('floating', 'assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+  myAnimation.offY = 18;
+
+  ghost.addAnimation('moving', 'assets/ghost_walk0001.png', 'assets/ghost_walk0004.png');
+
+  bg = new Group();
+
+  //create some background for visual reference
+  for(var i=0; i<80; i++)
+  {
+    //create a sprite and add the 3 animations
+    var rock = createSprite(random(-width, SCENE_W+width), random(-height, SCENE_H+height));
+    //cycles through rocks 0 1 2
+    rock.addAnimation('normal', 'assets/rocks'+i%3+'.png');
+    bg.add(rock);
+  }
+
+  frame = loadImage('assets/frame.png');
 }
 
 function draw() {
   background(255, 255, 255);
 
+  //mouse trailer, the speed is inversely proportional to the mouse distance
+  ghost.velocity.x = (camera.mouseX-ghost.position.x)/20;
+  ghost.velocity.y = (camera.mouseY-ghost.position.y)/20;
 
   //a camera is created automatically at the beginning
 
@@ -32,10 +51,27 @@ function draw() {
   camera.position.x = ghost.position.x;
   camera.position.y = ghost.position.y;
 
+  //limit the ghost movements
+  if(ghost.position.x < 0)
+    ghost.position.x = 0;
+  if(ghost.position.y < 0)
+    ghost.position.y = 0;
+  if(ghost.position.x > SCENE_W)
+    ghost.position.x = SCENE_W;
+  if(ghost.position.y > SCENE_H)
+    ghost.position.y = SCENE_H;
 
+  //draw the scene
+  //rocks first
+  drawSprites(bg);
 
-
-
+  //shadow using p5 drawing
+  noStroke();
+  fill(0, 0, 0, 20);
+  //shadow
+  ellipse(ghost.position.x, ghost.position.y+90, 80, 30);
+  //character on the top
+  drawSprite(ghost);
 
   //I can turn on and off the camera at any point to restore
   //the normal drawing coordinates, the frame will be drawn at
