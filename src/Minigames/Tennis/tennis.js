8d7dea4ba1deab1f0fa8fa79749f30f1d35c58e2
Maxwell Giffin <57154880+maxg66@users.noreply.github.com>
2019-11-02 15:15:17 -0400

    Update tennis.js

--- src/Minigames/Tennis/tennis.js
+++ src/Minigames/Tennis/tennis.js
@@ -10,6 +10,7 @@ var score = 0;
 function preload() {
   // preload() runs once
   court = loadImage('court.JPG');
+  court.resize(w, h);
 }
 
 function setup() { 
@@ -24,37 +25,37 @@ function setup() {
 function draw() { 
   background(court);
   
-//   // When the ball passes either side of the canvas, TURNAROUND
-//   if(positionX > width || positionX < 0) { 
-//     xSpeed = xSpeed * -1; 
-//   }
+  // When the ball passes either side of the canvas, TURNAROUND
+  if(positionX > width || positionX < 0) { 
+    xSpeed = xSpeed * -1; 
+  }
     
-//   if(positionY < 0) { 
-//     ySpeed = ySpeed * -1; 
-//   }
+  if(positionY < 0) { 
+    ySpeed = ySpeed * -1; 
+  }
 
-//   if((positionY>(h-35)&&positionY<h-25)&&(positionX>mouseX&&positionX<mouseX+60)){
-//     ySpeed = ySpeed * -1.15;
-//     xSpeed = xSpeed*1.15;
-//     score = score+1;
+  if((positionY>(h-35)&&positionY<h-25)&&(positionX>mouseX&&positionX<mouseX+60)){
+    ySpeed = ySpeed * -1.15;
+    xSpeed = xSpeed*1.15;
+    score = score+1;
 
-//   }
+  }
 
-//   if((positionY<h&&positionY>h-32)&&((positionX>mouseX&&positionX<mouseX+2)||positionX>mouseX+58&&positionX<mouseX+60)){
-//     xSpeed = xSpeed * -1; 
+  if((positionY<h&&positionY>h-32)&&((positionX>mouseX&&positionX<mouseX+2)||positionX>mouseX+58&&positionX<mouseX+60)){
+    xSpeed = xSpeed * -1; 
 
-//   }
+  }
 
-//   // if(ySpeed>0&&(positionY<h-15&&positionY>h-100)){
-//   //   background('green');
-//   // }
+  // if(ySpeed>0&&(positionY<h-15&&positionY>h-100)){
+  //   background('green');
+  // }
 
-//   // if(positionY>h){
-//   //   background('red');
-//   // }
-//  positionX = positionX + xSpeed;  
-//  positionY = positionY - ySpeed;
-//   ellipse(positionX, positionY, 10, 10);
-//   rect(0,mouseY,30,60);
-//   // text(score.toString(10),mouseX+27,h-25,60,30);
+  // if(positionY>h){
+  //   background('red');
+  // }
+ positionX = positionX + xSpeed;  
+ positionY = positionY - ySpeed;
+  ellipse(positionX, positionY, 10, 10);
+  rect(0,mouseY,30,60);
+  // text(score.toString(10),mouseX+27,h-25,60,30);
 }
